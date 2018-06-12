d90a8c56e2275398c72f09b9edeb44cc0fa3aea1
Orieoma Idam <oridam2005@gmail.com>
2018-06-12 17:53:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "Hello OG MONEY!"
+end
+
+get '/hometown' do
+  "from Ontario, Canada, OG MONEY!"
+end
+
+get '/favorite-song' do
+  "My favorite song is Motiv8 by J. Cole"
+end
+
 end
